54b268578a1c5179ceb55550a0140d570e9c56b6
Ansh Kevadiya <146837647+iamanshkevadiya@users.noreply.github.com>
2024-08-05 09:48:02 +0530

    Delete MVC directory

--- MVC/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const { getUser, createUser } = require("../controllers/user.controller")
-const userRouter = Router()
-userRouter.get("/",getUser)
-userRouter.post("/",createUser)
-
-
-
-module.exports=userRouter
